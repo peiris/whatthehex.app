2652c323df69a9a0fef8ae722511f4f569c5c873
Kasun Peiris <me@kasun.io>
2020-06-15 04:02:11 +0530

    GA integration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Home from 'components/home/home';
 import Navigation from 'components/navigation/navigation';
 import Sidebar from 'components/sidebar/sidebar';
 import React from "react";
+import ReactGA from 'react-ga';
 import Store from 'store';
 
+ReactGA.initialize('UA-169340778-1');
+ReactGA.pageview('/homepage');
+
 const App = () => {
-  // useEffect(() => {
-  //   document.getElementById('body').className = 'dark-mode';
-  // }, []);
-  
   return (
     <Store>
       <Navigation />
