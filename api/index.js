a8ebef1be82d71f88adf306e3edd090fb27e8e0b
Kasun Peiris <me@kasun.io>
2021-02-04 20:27:40 +0530

    Testing API

--- api/index.js
+++ api/index.js
@@ -1,5 +1,48 @@
 const app = require("express")();
 const { v4 } = require("uuid");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "hello@karapincha.io",
+    pass: "bhwtkidouzspcjrn",
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages!");
+  }
+});
+
+app.post("/access", jsonParser, (req, res, next) => {
+  const mail = {
+    from: "WhatTheHex Feedback <whatthehex@karapincha.io>",
+    to: "hello@karapincha.io",
+    message: req.body.message,
+    subject: "WhatTheHex Feedback - " + new Date(),
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
+});
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
